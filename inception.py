fc131b1394473ed3382e4bea18eea75d17b80d88
TheMrGhostman <34610021+TheMrGhostman@users.noreply.github.com>
2020-02-24 13:17:11 +0100

    Create inception.py

---
+++ inception.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn as nn
+
+
+def correct_sizes(sizes):
+    corrected_sizes = [s if s % 2 != 0 else s - 1 for s in sizes]
+    return corrected_sizes
+
+
+def pass_through(X):
+	return X
+
+
+class Inception(nn.Module):
+	def __init__(self, in_channels, n_filters, kernel_sizes=[9, 19, 39], bottleneck_channels=1, use_bottleneck=True, activation=nn.ReLU()):
+		"""
+		: param in_channels				Number of input channels (input features)
+		: param n_filters				Number of filters per convolution layer => out_channels = 4*n_filters
+		: param kernel_sizes			List of kernel sizes for each convolution.
+										Each kernel size must be odd number that meets -> "kernel_size % 2 !=0".
+										This is nessesery because of padding size.
+										For correction of kernel_sizes use function "correct_sizes". 
+		: param bottleneck_channels		Number of output channels in bottleneck. 
+										Bottleneck wont be used if nuber of in_channels is equal to 1.
+		: param use_bottleneck			Boolean variable. Use bottleneck or not.
+		: param activation				Activation function for output tensor (nn.ReLU()). 
+		"""
+		super(Inception, self).__init__()
+
+		if use_bottleneck and in_channels > 1:
+			self.bottleneck = nn.Conv1d(
+										in_channels=in_channels, 
+										out_channels=bottleneck_channels, 
+										kernel_size=1, 
+										stride=1, 
+										bias=False) 
+		else:
+			self.bottleneck = pass_through
+
+		self.conv_from_bottleneck_1 = nn.Conv1d(
+												in_channels=bottleneck_channels, 
+												out_channels=n_filters, 
+												kernel_size=kernel_sizes[0], 
+												stride=1, 
+												padding=kernel_sizes[0]//2, 
+												bias=False)
+		self.conv_from_bottleneck_2 = nn.Conv1d(
+												in_channels=bottleneck_channels, 
+												out_channels=n_filters, 
+												kernel_size=kernel_sizes[1], 
+												stride=1, 
+												padding=kernel_sizes[1]//2, 
+												bias=False)
+		self.conv_from_bottleneck_3 = nn.Conv1d(
+												in_channels=bottleneck_channels, 
+												out_channels=n_filters, 
+												kernel_size=kernel_sizes[2], 
+												stride=1, 
+												padding=kernel_sizes[2]//2, 
+												bias=False)
+		self.max_pool = nn.MaxPool1d(kernel_size=3, stride=1, padding=1)
+		self.conv_from_maxpool = nn.Conv1d(
+											in_channels=in_channels, 
+											out_channels=n_filters, 
+											kernel_size=1, 
+											stride=1,
+											padding=0, 
+											bias=False)
+		self.batch_norm = nn.BatchNorm1d(num_features=4*n_filters)
+		self.activation = activation
+
+	def forward(self, X):
+		# step 1
+		Z_bottleneck = self.bottleneck(X)
+		Z_maxpool = self.max_pool(X)
+		# step 2
+		Z1 = self.conv_from_bottleneck_1(Z_bottleneck)
+		Z2 = self.conv_from_bottleneck_2(Z_bottleneck)
+		Z3 = self.conv_from_bottleneck_3(Z_bottleneck)
+		Z4 = self.conv_from_maxpool(Z_maxpool)
+		print(Z1.shape, Z2.shape, Z3.shape, Z4.shape)
+		# step 3 
+		Z = torch.cat([Z1, Z2, Z3, Z4], axis=1)
+		Z = self.activation(self.batch_norm(Z))
+		return Z
+
+
